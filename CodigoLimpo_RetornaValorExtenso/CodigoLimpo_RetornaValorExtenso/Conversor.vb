e277d89cc3921bb6d6cd659f66a77e64143f2640
nataliastavares <38811449+nataliastavares@users.noreply.github.com>
2018-04-28 10:55:39 -0300

    0a19

---
+++ CodigoLimpo_RetornaValorExtenso/CodigoLimpo_RetornaValorExtenso/Conversor.vb
@@ -0,0 +1,57 @@
+﻿'Classe utilizada para converter 
+'numeros informados em extenso
+
+Public Class Conversor
+
+    'Função que converte números entre 0 e 19
+    Public Function Numeros0e19(ByVal dNumero As Decimal) As String
+        Dim sNumeroExtenso As String = ""
+
+        Select Case dNumero
+
+            Case 0
+                sNumeroExtenso = "zero"
+            Case 1
+                sNumeroExtenso = "um"
+            Case 2
+                sNumeroExtenso = "dois"
+            Case 3
+                sNumeroExtenso = "três"
+            Case 4
+                sNumeroExtenso = "quatro"
+            Case 5
+                sNumeroExtenso = "cinco"
+            Case 6
+                sNumeroExtenso = "seis"
+            Case 7
+                sNumeroExtenso = "sete"
+            Case 8
+                sNumeroExtenso = "oito"
+            Case 9
+                sNumeroExtenso = "nove"
+            Case 10
+                sNumeroExtenso = "dez"
+            Case 11
+                sNumeroExtenso = "onze"
+            Case 12
+                sNumeroExtenso = "doze"
+            Case 13
+                sNumeroExtenso = "treze"
+            Case 14
+                sNumeroExtenso = "quatorze"
+            Case 15
+                sNumeroExtenso = "quinze"
+            Case 16
+                sNumeroExtenso = "dezesseis"
+            Case 17
+                sNumeroExtenso = "dezessete"
+            Case 18
+                sNumeroExtenso = "dezoito"
+            Case 19
+                sNumeroExtenso = "dezenove"
+        End Select
+        Return sNumeroExtenso
+    End Function
+
+
+End Class
---
+++ CodigoLimpo_RetornaValorExtenso/CodigoLimpo_RetornaValorExtenso/Principal.vb
@@ -0,0 +1,24 @@
+﻿Module Principal
+
+    Sub Main()
+
+        Dim objConversor As New Conversor
+        Dim bNumero As Decimal
+        Dim sNumeroExtenso As String
+
+        'Recebe um número pelo usuário 
+        Console.Write("Digite um número em reais: ")
+        'Faz a leitura do numero do usuario
+        bNumero = Console.ReadLine()
+        sNumeroExtenso = objConversor.Numeros0e19(bNumero)
+        Console.WriteLine("Valor por Extenso: " + sNumeroExtenso)
+
+        Console.WriteLine("")
+        Console.WriteLine("")
+        Console.WriteLine("Group Software - 2018 ")
+        bNumero = Console.ReadLine()
+    End Sub
+
+
+
+End Module
