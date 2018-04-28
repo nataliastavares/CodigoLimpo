d445e954dc1204f4f207e5a1491a3df197416244
nataliastavares <38811449+nataliastavares@users.noreply.github.com>
2018-04-28 10:16:36 -0300

    Projeto Codigo limpo

---
+++ CodigoLimpo_RetornaValorExtenso/CodigoLimpo_RetornaValorExtenso/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("CodigoLimpo_RetornaValorExtenso")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("GROUP-SCCM01")> 
+<Assembly: AssemblyProduct("CodigoLimpo_RetornaValorExtenso")> 
+<Assembly: AssemblyCopyright("Copyright © GROUP-SCCM01 2018")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6f50cc04-0175-4a2e-8b78-4732338d1e87")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
